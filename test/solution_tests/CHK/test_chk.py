abd254a07d062b52f3bc59ac9dc247cba65046e3
sbx_user1051 <sbx_user1051@169.254.5.5>
2022-05-04 14:29:40 +0000

    Wed May 04 14:29:40 UTC 2022

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -7,3 +7,13 @@ class TestSum():
 
     def test_checkout_invalid_input(self):
         assert checkout_solution.checkout("ABCAABBDa") == -1
+
+    def test_checkout_two_b_discount(self):
+        assert checkout_solution.checkout("BB") == 45
+
+    def test_checkout_three_a_discount(self):
+        assert checkout_solution.checkout("AAA") == 130
+
+    def test_checkout_five_a_discount(self):
+        assert checkout_solution.checkout("AAAAA") == 200
+
